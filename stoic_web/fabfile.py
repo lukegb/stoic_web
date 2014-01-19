a83fa492e93853540e1f21160cc2308cd3511ab2
Joe <entrepreneurj@gmail.com>
2014-01-19 01:10:30 +0000

    Corrects requirements.txt ralative path bug in fabfile

--- stoic_web/fabfile.py
+++ stoic_web/fabfile.py
@@ -1,6 +1,8 @@
 from fabric.api import local
-
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 def commit():
-	local('pip freeze > ../requirements.txt')
+	local('pip freeze > '+BASE_DIR+'/requirements.txt')
 	local('git add --interactive && git commit')
