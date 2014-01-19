bb1c682fa472a5a03a82412bbd892d9be013f65c
Joe <entrepreneurj@gmail.com>
2014-01-19 09:57:03 +0000

    Adds runserver command to fabfilex

--- stoic_web/fabfile.py
+++ stoic_web/fabfile.py
@@ -22,3 +22,7 @@ def migrate(app_name):
 		result=	local('python '+BASE_DIR+'/stoic_web/manage.py schemamigration '+app_name+' --auto')
 	if result.failed and not confirm("The migration of '"+app_name+"' has failed. Would you like to continue anyway?"):
 		abort("Aborting process")
+
+def runserver():
+	test()
+	loccal('python '+BASE_DIR+'/stoic_web/manage.py runserver 0.0.0.0:8000')
